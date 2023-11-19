f5467fa2d21dce27faf511d05496cfb7b7a90671
Valentin Dury <valentin03dury@gmail.com>
2023-11-19 21:32:41 +0100

    [REFACTO] Put code inside class
    Signed-off-by: Valentin Dury <valentin03dury@gmail.com>

--- index.py
+++ index.py
@@ -1,66 +1,77 @@
 #!/usr/bin/python3
 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 import random
 import os
 
-# List all student
-students = os.listdir("pics")
-students.remove(".gitkeep")
+class Game(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
 
-root = Tk()
-root.title("Feet to Meters")
+        self.peoples = self.listPeople("pics")
+        self.create_widgets()
 
-correct_student = 0
+    @staticmethod
+    def listPeople(path: str):
+        # List all student
+        peoples = os.listdir("pics")
+        peoples.remove(".gitkeep")
+        return peoples
+
+    def create_widgets(self):
+
+        root.title("Who are you")
+
+        self.correct_person = 0
+
+        # create a label at center of window with image
+        self.myimage = ttk.Label(root)
+        self.myimage.grid(row=0, column=0, columnspan=3)
+
+        # Create 3 text variable to store the name
+        self.name1 = tk.StringVar()
+        self.name2 = tk.StringVar()
+        self.name3 = tk.StringVar()
+
+        # Create button
+        button1 = ttk.Button(root, width=30)
+        button2 = ttk.Button(root, width=30)
+        button3 = ttk.Button(root, width=30)
+
+        self.on_click(0)
+
+        # Create 3 button to guess the correct name
+
+        button1["textvariable"] = self.name1
+        button1["command"] = lambda: self.on_click(0)
+        button1.grid(row=1, column=0, padx=10, pady=10)
+
+        button2["textvariable"] = self.name2
+        button2["command"] = lambda: self.on_click(1)
+        button2.grid(row=1, column=1, padx=10, pady=10)
+
+        button3["textvariable"] = self.name3
+        button3["command"] = lambda: self.on_click(2)
+        button3.grid(row=1, column=2, padx=10, pady=10)
+
+    def on_click(self, choice: str):
+        if (choice == self.correct_person):
+            print("Correct")
+        else:
+            print("Wrong")
+            return
+        self.correct_person = random.randint(0, 2)
+        names = [random.choice(self.peoples),random.choice(self.peoples),random.choice(self.peoples)]
+        self.name1.set(names[0])
+        self.name2.set(names[1])
+        self.name3.set(names[2])
+        global imgobj
+        imgobj = ImageTk.PhotoImage(Image.open("pics/" + names[self.correct_person]))
+        self.myimage["image"] = imgobj
 
 
-# create a label at center of window with image
-myimage = ttk.Label(root)
-myimage.grid(row=0, column=0, columnspan=3)
-
-# Create 3 text variable to store the name
-name1 = StringVar()
-name2 = StringVar()
-name3 = StringVar()
-
-# Create button
-button1 = ttk.Button(root, width=30)
-button2 = ttk.Button(root, width=30)
-button3 = ttk.Button(root, width=30)
-
-def on_click(choice: str):
-    global correct_student
-    if (choice == correct_student):
-        print("Correct")
-    else:
-        print("Wrong")
-        return
-    correct_student = random.randint(0, 2)
-    names = [random.choice(students),random.choice(students),random.choice(students)]
-    name1.set(names[0])
-    name2.set(names[1])
-    name3.set(names[2])
-    global imgobj
-    imgobj = ImageTk.PhotoImage(Image.open("pics/" + names[correct_student]))
-    myimage["image"] = imgobj
-
-on_click(0)
-
-# Create 3 button to guess the correct name
-
-button1["textvariable"] = name1
-button1["command"] = lambda: on_click(0)
-button1.grid(row=1, column=0, padx=10, pady=10)
-
-button2["textvariable"] = name2
-button2["command"] = lambda: on_click(1)
-button2.grid(row=1, column=1, padx=10, pady=10)
-
-button3["textvariable"] = name3
-button3["command"] = lambda: on_click(2)
-button3.grid(row=1, column=2, padx=10, pady=10)
-
-
-root.mainloop()
+root = tk.Tk()
+myapp = Game(root)
+myapp.mainloop()
